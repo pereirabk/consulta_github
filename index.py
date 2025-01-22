fc1c76593c0bb9d4864ccffd66afe72999532796
pereirabk <pereirabkwork@gmail.com>
2025-01-22 16:51:12 -0300

    Atualizacao teste

--- index.py
+++ index.py
@@ -14,7 +14,7 @@ def selecionarUsuario(username):
 def ui():
     st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">',unsafe_allow_html=True)
 
-    st.title('Consulta GITHUB')
+    st.title('Consulta GITHUB!')
     username = st.text_input('Insira o USERNAME de usuario do GITHUB')
 
     if st.button('Buscar'):
